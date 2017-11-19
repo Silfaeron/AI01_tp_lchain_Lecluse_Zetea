f167a98ef48280ba9257d8eb0194efcc9334d0fa
Lucas Zetea <lucas@c1mrr11xdv30-lucas.home>
2017-11-19 16:11:55 +0100

    New compare

--- src/tp3.c
+++ src/tp3.c
@@ -165,7 +165,7 @@ int compare(char *str1, char *str2){
 		return 2;
 
 	int i;
-	for(i=0; *(str1+i) != '\n'; i++){
+	for(i=0; *(str1+i) != '\0'; i++){
 		if(*(str1+i)-'0' > *(str2+i)-'0')
 			return 1;
 		else if(*(str1+i)-'0' < *(str2+i)-'0')
